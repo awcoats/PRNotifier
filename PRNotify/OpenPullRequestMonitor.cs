0292f8246b678d7c429bd1db662b6d49c16bc232
Andrew Coats <acoats@calicosol.com>
2016-01-19 19:15:24 -0700

    intial checkin.

---
+++ PRNotify/OpenPullRequestMonitor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Media;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using System.Timers;
+using Octokit;
+
+namespace PRNotify
+{
+    public class OpenPullRequestMonitor
+    {
+        private readonly Timer _timer;
+        private PullRequest _pr;
+
+        public OpenPullRequestMonitor()
+        {
+            var pollingPeriod = TimeSpan.FromMinutes(1).TotalMilliseconds;
+            _timer = new Timer(pollingPeriod) {AutoReset = true};
+            _timer.Elapsed += (sender, eventArgs) =>
+            {
+                var prs = 0;
+                Task.Run(async () =>
+                {
+                    prs = await PollGitHub();
+                    Console.WriteLine("Polled PR count:" + prs);
+                }).Wait();
+
+                if (prs > OpenPrs)
+                {
+                    var soundPlayer = new SoundPlayer("fanfare3.wav");
+                    soundPlayer.Play();
+                    if (_pr != null)
+                    {
+                        var speech = new SpeechSynthesizer();
+                        speech.Speak("New pull request from " +_pr.User.Name);
+                    }
+
+                    OpenPrs = prs;
+                }
+                else if (prs < OpenPrs)
+                {
+                    OpenPrs = prs;
+                }
+            };
+        }
+
+        public int OpenPrs { get; private set; }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+
+        public async Task<int> PollGitHub()
+        {
+            var gitHubClient = new GitHubClient(new ProductHeaderValue("repo"));
+            gitHubClient.Credentials = new Credentials("****", "****");
+            var openPullRequests = new PullRequestRequest {State = ItemState.Open};
+            var prs = await gitHubClient.PullRequest.GetAllForRepository("owner", "reponame", openPullRequests);
+            if (prs.Count > 0)
+            {
+                _pr = prs[0];
+            }
+            return prs.Count;
+        }
+    }
+}
---
+++ PRNotify/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Media;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+using Topshelf;
+
+namespace PRNotify
+{  
+    public class Program
+    {
+     
+        public static  void Main(string[] args)
+        {
+
+            //var monitor = new OpenPullRequestMonitor();
+            //monitor.Start();
+
+            //Console.WriteLine("Press enter to finish");
+            //Console.ReadLine();
+            HostFactory.Run(x => //1
+            {
+                x.Service<OpenPullRequestMonitor>(s => //2
+                {
+                    x.StartAutomatically(); // Start the service automatically
+                    s.ConstructUsing(name => new OpenPullRequestMonitor()); //3
+                    s.WhenStarted(tc => tc.Start()); //4
+                    s.WhenStopped(tc => tc.Stop()); //5
+                });
+                x.RunAsLocalSystem(); //6
+
+                x.SetDescription("CalPEATS GitHub PR Monitor"); //7
+                x.SetDisplayName("CalPEATS PR Monitor"); //8
+                x.SetServiceName("CalPEATSPRMon");
+            });
+        }
+    }
+}
